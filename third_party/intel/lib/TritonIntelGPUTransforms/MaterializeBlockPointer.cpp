6d64ebe98fa2e2e06e91c5b92d337566915256de
Lu,Chengjun <chengjun.lu@intel.com>
2026-05-08 11:17:43 +0000

    Code change to support tt.descriptor_gather lowering.
    Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

--- third_party/intel/lib/TritonIntelGPUTransforms/MaterializeBlockPointer.cpp
+++ third_party/intel/lib/TritonIntelGPUTransforms/MaterializeBlockPointer.cpp
@@ -73,6 +73,9 @@ public:
       visit(op, axisInfoAnalysis, strideAnalysis, context);
     mod.walk(
         [&](tt::DescriptorLoadOp op) { visit(op, axisInfoAnalysis, context); });
+    mod.walk([&](tt::DescriptorGatherOp op) {
+      visit(op, axisInfoAnalysis, context);
+    });
     mod.walk([&](tt::DescriptorStoreOp op) {
       visit(op, axisInfoAnalysis, context);
     });
@@ -81,6 +84,12 @@ public:
 private:
   // Visit method for descriptor operations
   void visit(tt::DescriptorLoadOp op,
+             tt::intel::ModuleAxisInfoAnalysis &axisInfoAnalysis,
+             MLIRContext *context) const {
+    visitDescriptor(op, op.getResult().getType(), axisInfoAnalysis, context);
+  }
+
+  void visit(tt::DescriptorGatherOp op,
              tt::intel::ModuleAxisInfoAnalysis &axisInfoAnalysis,
              MLIRContext *context) const {
     visitDescriptor(op, op.getResult().getType(), axisInfoAnalysis, context);
@@ -132,9 +141,12 @@ private:
     if (rank == 1)
       return;
 
-    if (!satisfies2DBlockReadAlignment(op, axisInfoAnalysis)) {
-      LDBG("Alignment checks failed for: " << *op);
-      return;
+    if constexpr (llvm::is_one_of<OpType, tt::DescriptorLoadOp,
+                                  tt::DescriptorStoreOp>::value) {
+      if (!satisfies2DBlockReadAlignment(op, axisInfoAnalysis)) {
+        LDBG("Alignment checks failed for: " << *op);
+        return;
+      }
     }
 
     unsigned elementWidth = tensorType.getElementTypeBitWidth();
