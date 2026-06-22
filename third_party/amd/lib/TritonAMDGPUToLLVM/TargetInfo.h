intel/intel-xpu-backend-for-triton

Lower the horizontal batched reduce of matrix with optimal inline VISA.
Signed-off-by: Lu,Chengjun <chengjun.lu@intel.com>

third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
@@ -56,6 +56,13 @@ public:
                   triton::ReduceOp op, unsigned numLaneToReduce,
                   unsigned interleave) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       std::map<SmallVector<unsigned>, SmallVector<Value>> &acc,
+                       triton::ReduceOp op, unsigned numLaneToReduce,
+                       unsigned interleave) const override {
+    return false;
+  };
+
   std::string getMulhiFuncName(Type resultElementTy) const override;
 
   void printf(RewriterBase &rewriter, Value formatStrStart,

third_party/intel/lib/TritonIntelGPUToLLVM/TargetInfo.h
@@ -50,6 +50,11 @@ public:
   Value programId(RewriterBase &rewriter, Location loc, ModuleOp moduleOp,
                   ProgramIDDim axis) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       std::map<SmallVector<unsigned>, SmallVector<Value>> &acc,
+                       triton::ReduceOp op, unsigned numLaneToReduce,
+                       unsigned interleave) const override;
+
   bool warpReduce(RewriterBase &rewriter, Location loc, SmallVector<Value> &acc,
                   triton::ReduceOp op, unsigned numLaneToReduce,
                   unsigned interleave) const override;
@@ -92,5 +97,6 @@ private:
 };
 
 std::unique_ptr<TargetInfo> createTargetInfo(ModuleOp mod);
+
 } // namespace mlir::triton::intel
 #endif // TRITON_CONVERSION_TRITONGPU_TO_LLVM_TARGETINFOINTEL_H

third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.h
@@ -49,6 +49,13 @@ public:
                   triton::ReduceOp op, unsigned numLaneToReduce,
                   unsigned interleave) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       std::map<SmallVector<unsigned>, SmallVector<Value>> &acc,
+                       triton::ReduceOp op, unsigned numLaneToReduce,
+                       unsigned interleave) const override {
+    return false;
+  };
+
   std::string getMulhiFuncName(Type resultElementTy) const override;
 
   void printf(RewriterBase &rewriter, Value formatStrStart,

gurwinderintel/intel-xpu-backend-for-triton-1

third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
@@ -78,6 +78,13 @@ public:
                   triton::ReduceOp op,
                   unsigned reduceLaneIdMask) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       SmallVector<SmallVector<Value>> &acc,
+                       triton::ReduceOp op,
+                       unsigned reduceLaneIdMask) const override {
+    return false;
+  };
+
   std::string getMulhiFuncName(Type resultElementTy) const override;
 
   void printf(RewriterBase &rewriter, Value formatStrStart,

third_party/intel/lib/TritonIntelGPUToLLVM/TargetInfo.h
@@ -54,6 +54,11 @@ public:
   Value programId(RewriterBase &rewriter, Location loc, ModuleOp moduleOp,
                   ProgramIDDim axis) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       SmallVector<SmallVector<Value>> &acc,
+                       triton::ReduceOp op,
+                       unsigned reduceLaneIdMask) const override;
+
   bool warpReduce(RewriterBase &rewriter, Location loc, SmallVector<Value> &acc,
                   triton::ReduceOp op,
                   unsigned reduceLaneIdMask) const override;
@@ -95,5 +100,6 @@ private:
 };
 
 std::unique_ptr<TargetInfo> createTargetInfo(ModuleOp mod);
+
 } // namespace mlir::triton::intel
 #endif // TRITON_CONVERSION_TRITONGPU_TO_LLVM_TARGETINFOINTEL_H

third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.h
@@ -66,6 +66,13 @@ public:
                   triton::ReduceOp op,
                   unsigned reduceLaneIdMask) const override;
 
+  bool warpBatchReduce(RewriterBase &rewriter, Location loc,
+                       SmallVector<SmallVector<Value>> &acc,
+                       triton::ReduceOp op,
+                       unsigned reduceLaneIdMask) const override {
+    return false;
+  };
+
   std::string getMulhiFuncName(Type resultElementTy) const override;
 
   void printf(RewriterBase &rewriter, Value formatStrStart,